17b9e49d3247f2702deb3707ef0416868fc63c15
Michał Wypych <mwypych@agh.edu.pl>
2018-04-19 14:41:35 +0200

    Adds stub of memleak detector under macos

---
+++ tests/lib/memleakmacos/MemLeakTest.cpp
@@ -0,0 +1,10 @@
+//
+// Created by mwypych on 21.02.17.
+//
+
+#include <iostream>
+#include "MemLeakTest.h"
+
+MemLeakTest::MemLeakTest() {
+  std::cout << "On Mac platform Memory Leak detection library is currently unavailable!" << std::endl;
+}
